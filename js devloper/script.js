7577a0fd11215a4a0ebe8f4c0b0978d2d0c378b0
AsmathHimendhar <103575371+AsmathHimendhar@users.noreply.github.com>
2022-05-23 16:46:16 +0530

    Add files via upload

---
+++ js devloper/script.js
@@ -0,0 +1,122 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+// const x = 23;
+// let f = a => b - 3;
+// console.log(x);
+// var x = [22, 33, 44, 5, 6, 7];
+// // const fun = function (x) {
+// for (var i = 0; i < x.length; i++) {
+//   x = x + i; //`...${x[i]}^C.in.${i + 1}.days`;
+// }
+// console.log(x);
+// };
+//fun(deg);
+// let n = [17, 21, 23];
+// let z = [33, 56, 78, 90, 1];
+// const m = [];
+// //let j = `...${n[0]}^c in 1 days`;
+// const l = function (hh) {
+//   let j = "";
+//   for (let i = 0; i < hh.length; i++) {
+//     //   if (j === 0) continue;
+//     // j +
+//     j += `...${hh[i]}^C in ${i + 1} days`;
+//     // m.push(j);
+//   }
+//   console.log(j + "...");
+// };
+// l(z);
+//let m = j.slice();
+// const a = [1, 2, 3, 4];
+// console.log(a);
+// let [b, , d] = a;
+// console.log(b, d);
+// [b, d] = [d, b];
+// console.log(b, d);
+// let n = prompt("up to");
+// let x = [];
+// let m = [];
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2) {
+//     m.push(i);
+//   } else {
+//     x.push(i);
+//   }
+// }
+// console.log(`odd nums: ${m}`);
+// console.log(`even nums: ${x}`);
+// let n = 123;
+// const r = 0;
+// n = n / 10;
+// const a = r * 10 + (n % 10);
+// console.log(a);
+// let n = [1, 2, 3, 4, 5];
+// n.reverse();
+// console.log(n);
+// let a = 2;
+// let b = 2;
+// let c = 2;
+// let d = 4;
+
+// if (a == b) {
+
+//   if (a == c) {
+
+//     if (c == d) {
+//       console.log(`1`);
+//     } else {
+//       console.log(`a`);
+//     }
+//   } else {
+//     console.log("b");
+//   }
+// } else {
+//   console.log("c");
+// }
+// const y = 5;
+// for (let i = 0; i <= y; i++) {
+//   //   for (let j = 0; j < y; j++) {
+//   // if (i == j || i + j == y - 1) {
+//   console.log("*".repeat(i));
+// }
+// //   }
+// //}
+// let str2 = "";
+// const x = 5;
+// const y = 5;
+// for (let i = 1; i <= y; i++) {
+//   for (let j = 1; j <= x; j++) {
+//     if (i <= j) {
+//       str2 = str2.concat("*");
+//     } else {
+//       str2 = str2.concat(" ");
+//     }
+//   }
+//   str2 = str2.concat("\n");
+// }
+//console.log(str2);
+//let arr = [1, 2]; //4
+//let i = a[0];function foo(array) {
+
+// const h = a.length;
+// const t = a;
+
+// for (let z = 0; z < a.length; z++) {
+//   for (let j = z + 1; j < a.length; j++) {
+//     if (a[z] === a[j]) {
+//       // console.log(a[z]);
+//     }
+//   }
+// }
+// console.log(h - t.length);
+// // a.pop(1, 1);
+// const n = 4;
+// let m = 1;
+// for (let i = n; i >= 1; i--) {
+//   console.log(i);
+//   m = m * i;
+// }
+// console.log(m);
+// let m = prompt("enter n.o");
+// m = m ** (1 / 2);
+// console.log(m);
